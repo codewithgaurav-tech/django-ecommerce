d30e27460a4e12f3bd8e8d51d2aca380e3b9af2d
codewithgaurav-tech <glalwani177@gmail.com>
2021-08-26 20:20:28 +0530

    second commit

--- shop/views.py
+++ shop/views.py
@@ -1,16 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Product
+from .models import Contact, Product
 from math import ceil
-
 # Create your views here.
 def index(request):
-    # products=Product.objects.all()
-    # print(products)
-    # n=len(products)
-    # nSlides= n//4 + ceil((n/4) + (n//4))
-    # # params={'no_of_slides':nSlides,'range':range(1,nSlides),'product':products,}
-    # allProds=[[products,range(1,len(products)),nSlides],[products,range(1,nSlides),nSlides]]
     allProds=[]
     catprods=Product.objects.values('category','id')
     cats={item['category'] for item in catprods}
@@ -21,20 +14,28 @@ def index(request):
         allProds.append([prod, range(1,nSlides),nSlides])
     params={'allProds':allProds}
     return  render(request,'shop/index.html' ,params)
-
 def about(request):
     return render(request,'shop/about.html')
 def contact(request):
+    if request.method=="POST":
+        print(request)
+        name=request.POST.get('name', '')
+        email=request.POST.get('email', '')
+        phone=request.POST.get('phone', '')
+        desc=request.POST.get('desc', '')
+        print(name,email,phone, desc )  
+        contact=Contact(name=name,email=email,phone=phone,desc=desc)
+        contact.save()
     return  render(request,'shop/contact.html')
 def tracker(request):
     return  render(request,'shop/tracker.html')
 def search(request):
     return  render(request,'shop/search.html')
-def products(request,myid):
-    product=Product.objects.filter(id=myid)
-    # product=Product.objects.all()
+def products(request,my_id):
+    print(my_id)
+    product=Product.objects.filter(id=my_id)
     print(product)
-    
-    return render(request, "shop/prodView.html",{'prod':product})
+    params={'prod':product}
+    return render(request, "shop/prodView.html",params)
 def checkout(request):
     return  render(request,'shop/checkout.html')
